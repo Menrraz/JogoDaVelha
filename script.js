4164c4312be36d3482a4a90d935ad08a0893fbee
Guilherme Menrraz <menrraz@gmail.com>
2022-11-28 15:22:49 -0300

    Adiciona dois botões: um e dois jogadores

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 (function (){
         let warning = document.querySelector('.warning').remove() // Remove warning about required javacript activation
+        document.querySelector('body').insertAdjacentHTML('beforeend', `
+            <section>
+                <input type='button' value="Um jogador">
+                <input type='button' value="Dois jogadores">
+            </section>
+        `)
     }
 )()

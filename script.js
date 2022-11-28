7cddd7b8e2f4be43afe1cdfca77b0748df16aa6d
Guilherme Menrraz <menrraz@gmail.com>
2022-11-28 15:41:19 -0300

    Adiciona uma classe ao section

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function (){
         let warning = document.querySelector('.warning').remove() // Remove warning about required javacript activation
         document.querySelector('body').insertAdjacentHTML('beforeend', `
-            <section>
+            <section class="start-section">
                 <input type='button' value="Um jogador">
                 <input type='button' value="Dois jogadores">
             </section>

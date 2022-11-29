cd6c8afcd9353188cec24a137c3aacdd5249c722
Guilherme Menrraz <menrraz@gmail.com>
2022-11-29 15:47:33 -0300

    Adiciona as nove células

--- twoplayers.js
+++ twoplayers.js
@@ -3,7 +3,15 @@ function twoPlayers() {
     let body = document.querySelector('body');
     body.insertAdjacentHTML('beforeend', `
         <section class='board'>
-        
+            <div class="cell cell1"></div>
+            <div class="cell cell2"></div>
+            <div class="cell cell3"></div>
+            <div class="cell cell4"></div>
+            <div class="cell cell5"></div>
+            <div class="cell cell6"></div>
+            <div class="cell cell7"></div>
+            <div class="cell cell8"></div>
+            <div class="cell cell9"></div>
         </section>
     `);
 }

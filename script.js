f27f20c1688e15daf8a8a1ac306c3339b3803cce
Guilherme Menrraz <menrraz@gmail.com>
2022-11-29 15:00:14 -0300

    Adiciona ids dos inputs

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
         let warning = document.querySelector('.warning').remove() // Remove warning about required javacript activation
         document.querySelector('body').insertAdjacentHTML('beforeend', `
             <section class="start-section">
-                <input type='button' value="Um jogador" onclick="onePlayer()">
-                <input type='button' value="Dois jogadores" onclick="twoPlayers()">
+                <input type='button' value="Um jogador" onclick="onePlayer()" id="onePlayer">
+                <input type='button' value="Dois jogadores" onclick="twoPlayers()" id="twoPlayer">
             </section>
         `)
     }

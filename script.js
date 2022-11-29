93e3311a73f2d0cce2b1f93c4dc3eee61a164f63
Guilherme Menrraz <menrraz@gmail.com>
2022-11-29 14:53:23 -0300

    Adiciona a chamada de duas funções nos botões

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
         let warning = document.querySelector('.warning').remove() // Remove warning about required javacript activation
         document.querySelector('body').insertAdjacentHTML('beforeend', `
             <section class="start-section">
-                <input type='button' value="Um jogador">
-                <input type='button' value="Dois jogadores">
+                <input type='button' value="Um jogador" onclick="onePlayer()">
+                <input type='button' value="Dois jogadores" onclick="twoPlayers()">
             </section>
         `)
     }

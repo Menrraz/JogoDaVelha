64040fd7fb5be3d1a9ff86cd11ef7e75102b211f
Guilherme Menrraz <menrraz@gmail.com>
2022-11-29 15:23:21 -0300

    Adiciona section board

--- twoplayers.js
+++ twoplayers.js
@@ -1,3 +1,9 @@
 function twoPlayers() {
     document.querySelector(".start-section").remove(); // Remove initial menu
+    let body = document.querySelector('body');
+    body.insertAdjacentHTML('beforeend', `
+        <section class='board'>
+        
+        </section>
+    `);
 }

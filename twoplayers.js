c87532977c127b19e9a355f511ec0154c6586a21
Guilherme Menrraz <menrraz@gmail.com>
2022-11-30 15:48:03 -0300

    Faz chamar a função play() após clicar na célula

--- twoplayers.js
+++ twoplayers.js
@@ -7,15 +7,15 @@ function createBoard() {
     let body = document.querySelector('body');
     body.insertAdjacentHTML('beforeend', `
         <section class='board'>
-            <div class="cell cell1"></div>
-            <div class="cell cell2"></div>
-            <div class="cell cell3"></div>
-            <div class="cell cell4"></div>
-            <div class="cell cell5"></div>
-            <div class="cell cell6"></div>
-            <div class="cell cell7"></div>
-            <div class="cell cell8"></div>
-            <div class="cell cell9"></div>
+            <div class="cell cell1" onclick="play(${player}, 'cell1')"></div>
+            <div class="cell cell2" onclick="play(${player}, 'cell2')"></div>
+            <div class="cell cell3" onclick="play(${player}, 'cell3')"></div>
+            <div class="cell cell4" onclick="play(${player}, 'cell4')"></div>
+            <div class="cell cell5" onclick="play(${player}, 'cell5')"></div>
+            <div class="cell cell6" onclick="play(${player}, 'cell6')"></div>
+            <div class="cell cell7" onclick="play(${player}, 'cell7')"></div>
+            <div class="cell cell8" onclick="play(${player}, 'cell8')"></div>
+            <div class="cell cell9" onclick="play(${player}, 'cell9')"></div>
         </section>
     `);
 }

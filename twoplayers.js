89e772c25b7e6552db7539f4da9d97c6a624144f
Guilherme Menrraz <menrraz@gmail.com>
2022-12-01 15:26:20 -0300

    Simplifica o parâmetro e facilita seu uso

--- twoplayers.js
+++ twoplayers.js
@@ -8,15 +8,15 @@ function createBoard() {
     let body = document.querySelector('body');
     body.insertAdjacentHTML('beforeend', `
         <section class='board'>
-            <div class="cell cell1" onclick="play('cell1')"></div>
-            <div class="cell cell2" onclick="play('cell2')"></div>
-            <div class="cell cell3" onclick="play('cell3')"></div>
-            <div class="cell cell4" onclick="play('cell4')"></div>
-            <div class="cell cell5" onclick="play('cell5')"></div>
-            <div class="cell cell6" onclick="play('cell6')"></div>
-            <div class="cell cell7" onclick="play('cell7')"></div>
-            <div class="cell cell8" onclick="play('cell8')"></div>
-            <div class="cell cell9" onclick="play('cell9')"></div>
+            <div class="cell cell1" onclick="play(1)"></div>
+            <div class="cell cell2" onclick="play(2)"></div>
+            <div class="cell cell3" onclick="play(3)"></div>
+            <div class="cell cell4" onclick="play(4)"></div>
+            <div class="cell cell5" onclick="play(5)"></div>
+            <div class="cell cell6" onclick="play(6)"></div>
+            <div class="cell cell7" onclick="play(7)"></div>
+            <div class="cell cell8" onclick="play(8)"></div>
+            <div class="cell cell9" onclick="play(9)"></div>
         </section>
     `);
 }

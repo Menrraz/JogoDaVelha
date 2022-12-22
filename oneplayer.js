45a46ebc02deeec09273e015938c30d6e6ff68f9
Guilherme Menrraz <menrraz@gmail.com>
2022-12-22 16:33:37 -0300

    Adiciona array das possibilidade de vitória

--- oneplayer.js
+++ oneplayer.js
@@ -3,5 +3,14 @@ function onePlayer() {
     createBoard('one');
 }
 function botPlay() {
-    
+    let wins = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [7,4,1],
+        [8,5,2],
+        [9,6,3],
+        [7,5,3],
+        [9,5,1],
+    ];
 }

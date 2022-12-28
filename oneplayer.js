532245d9ab6e6e6f02a0f62438cd65076a2ab553
Guilherme Menrraz <menrraz@gmail.com>
2022-12-28 10:58:08 -0300

    Impede de usuário de vencer contra o bot

--- oneplayer.js
+++ oneplayer.js
@@ -13,4 +13,22 @@ function botPlay() {
         [7,5,3],
         [9,5,1],
     ];
+    for (let i = 0; i < wins.length; i++) {
+        let xChance = 0; // Bot wants to stops this variable to get to 3, otherwise it loses.
+        for (let j = 0; j < playerXCells.length; j++) {
+            for (let k = 0; k < 3; k++) {
+                // Play where prevent user victory
+                if (wins[i][k] == playerXCells[j]) {
+                    xChance++;
+                }
+                if (xChance == 2 && player == 2) {
+                    let nextPlay = wins[i].filter(x => !playerXCells.includes(x)); 
+                    if (markedCells.indexOf(nextPlay[0]) == -1) {
+                        player = 2;
+                        play(nextPlay[0]);
+                    }
+                }
+            }
+        }
+    }
 }

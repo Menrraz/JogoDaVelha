419e357195adf827eff933a5e62eda36157ced4e
Guilherme Menrraz <menrraz@gmail.com>
2023-01-19 10:31:43 -0300

    Muda comentário de lugar para melhor visibilidade

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function start(){
-    document.querySelector('.warning').remove() // Remove warning about required javacript activation
+    // Remove warning about required javacript activation
+    document.querySelector('.warning').remove();
     document.querySelector('body').insertAdjacentHTML('beforeend', `
         <section class="start-section">
             <input type='button' value="Um jogador" onclick="onePlayer()" id="onePlayer">

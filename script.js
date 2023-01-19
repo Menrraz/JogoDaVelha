651379fa0012cca387f266231eb12e4381414f4c
Guilherme Menrraz <menrraz@gmail.com>
2023-01-19 10:20:57 -0300

    Resolve récuo

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function start(){
-        let warning = document.querySelector('.warning').remove() // Remove warning about required javacript activation
-        document.querySelector('body').insertAdjacentHTML('beforeend', `
-            <section class="start-section">
-                <input type='button' value="Um jogador" onclick="onePlayer()" id="onePlayer">
-                <input type='button' value="Dois jogadores" onclick="twoPlayers()" id="twoPlayer">
-            </section>
-        `)
-    }
+    let warning = document.querySelector('.warning').remove() // Remove warning about required javacript activation
+    document.querySelector('body').insertAdjacentHTML('beforeend', `
+        <section class="start-section">
+            <input type='button' value="Um jogador" onclick="onePlayer()" id="onePlayer">
+            <input type='button' value="Dois jogadores" onclick="twoPlayers()" id="twoPlayer">
+        </section>
+    `)
+}

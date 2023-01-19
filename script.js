8a6125b756f57a23dd9586950598074eb1c2431b
Guilherme Menrraz <menrraz@gmail.com>
2023-01-19 10:24:51 -0300

    Remove código desnecessário

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function start(){
-    let warning = document.querySelector('.warning').remove() // Remove warning about required javacript activation
+    document.querySelector('.warning').remove() // Remove warning about required javacript activation
     document.querySelector('body').insertAdjacentHTML('beforeend', `
         <section class="start-section">
             <input type='button' value="Um jogador" onclick="onePlayer()" id="onePlayer">

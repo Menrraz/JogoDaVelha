03df7f300d39f802bf79a4019d27d10626767c7d
Guilherme Menrraz <menrraz@gmail.com>
2023-01-19 11:44:27 -0300

    Envolve código no try

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function start(){
     // Remove warning about required javacript activation
-    document.querySelector('.warning').remove();
+    try {document.querySelector('.warning').remove();} catch {}
     document.querySelector('body').insertAdjacentHTML('beforeend', `
         <section class="start-section">
             <input type='button' value="Um jogador" onclick="onePlayer()" id="onePlayer">
